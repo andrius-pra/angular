d5fc5fbc8a516b7c2c486cefa95c149f32e89be3
Andrius <andrius.pranciliauskas@gmail.com>
2019-08-09 18:20:21 +0300

    feat(language-service): Completions for event bindings
    Resolves type of $event variable in a custom event binding. So language service can provide completions for $event type members.
    
    (cherry picked from commit 9f709a44d0a338c270386411f8208de33ed4aaa0)

--- packages/language-service/test/completions_spec.ts
+++ packages/language-service/test/completions_spec.ts
@@ -342,6 +342,26 @@ describe('completions', () => {
     //   expectContain(completions, CompletionKind.PROPERTY, ['innerText']);
     // });
   });
+
+  describe('custom event binding', () => {
+    it('should be able to get completions for event name', () => {
+      const marker = mockHost.getLocationMarkerFor(PARSING_CASES, 'custom-event-name');
+      const completions = ngLS.getCompletionsAt(PARSING_CASES, marker.start);
+      expectContain(completions, CompletionKind.ATTRIBUTE, ['(deleteRequest)']);
+    });
+
+    it('should be able to get completions for $event variable', () => {
+      const marker = mockHost.getLocationMarkerFor(PARSING_CASES, 'custom-event-binding');
+      const completions = ngLS.getCompletionsAt(PARSING_CASES, marker.start);
+      expectContain(completions, CompletionKind.VARIABLE, ['$event']);
+    });
+
+    it('should be able to get completions for custom event members', () => {
+      const marker = mockHost.getLocationMarkerFor(PARSING_CASES, 'custom-event-members');
+      const completions = ngLS.getCompletionsAt(PARSING_CASES, marker.start);
+      expectContain(completions, CompletionKind.PROPERTY, ['name', 'age', 'street']);
+    });
+  });
 });
 
 function expectContain(
